966460d4e751b8e5e1d8ffaec39df2a6ecec7d21
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-22 19:34:47 -0600

    fix: sometimes API gives back empty file

--- main.py
+++ main.py
@@ -22,11 +22,16 @@ if __name__ == "__main__":
     new_file = history_folder / datetime.now().strftime("%Y%m%dT%H.json")
 
     # API unreliable: sometimes returns JSON and sometimes a zipped file
-    if "gzip" in str(subprocess.check_output(["file", tmp_file])):
+    file_type = str(subprocess.check_output(["file", tmp_file]))
+    if "gzip" in file_type:
         L.info("got gzip file")
         jsn = util.gzip_to_json(tmp_file)
         json.dump(jsn, open(new_file, "w"))
         tmp_file.unlink()  # remove tmp file
+    elif "empty" in file_type:
+        L.error("received empty file!")
+        # TODO: handle empty file case
+        tmp_file.unlink()
     else:
         L.info("got json file")
         tmp_file.rename(new_file)
