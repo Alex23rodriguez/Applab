55a9420c1ed849321cb0128b6afe9ecaffd23ce3
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-22 21:38:53 -0600

    fix: fixed paths and added logging

--- table.py
+++ table.py
@@ -1,17 +1,23 @@
 from datetime import datetime, timedelta
 from pathlib import Path
 import pandas as pd
+import logging
 
-current_data = Path(__file__) / "history" / "current"
-tables_folder = Path(__file__) / "data_municipios"
+logging.basicConfig(level=logging.INFO)
+L = logging.getLogger(__name__)
+
+current_data = Path(__file__).parent / "history" / "current"
+tables_folder = Path(__file__).parent / "data_municipios"
 
 if __name__ == "__main__":
     dtnow = datetime.now()
     now = dtnow.strftime("%Y%m%dT%H")
     onehourago = (dtnow - timedelta(hours=1)).strftime("%Y%m%dT%H")
 
-    df: pd.DataFrame = pd.read_json(current_data)
+    L.info("reading latest data from API")
+    df = pd.read_json(current_data)
 
+    L.info("extracting values")
     # filter out unimportant data
     # hloc: hora local
     df = df[(df["hloc"] == now) | (df["hloc"] == onehourago)]
@@ -25,6 +31,7 @@ if __name__ == "__main__":
     # reset index to go back to normal dataframe
     df_prom = df_prom.reset_index()
 
+    L.info("merging with previous data")
     # merge with other data
     data = pd.read_csv(tables_folder / "data1.csv")
 
@@ -35,10 +42,12 @@ if __name__ == "__main__":
         left_on=["Cve_Ent", "Cve_Mun"],
         right_on=["ides", "idmun"],
     )
+    print(ans.head())
 
     # save this data
     new_file = f"{tables_folder / now}.csv"
-    ans.to_csv(new_file)
+    L.info(f"saving merged data as {new_file}")
+    ans.to_csv(new_file, index=False)
 
     # add symlink 'current'
     sym = tables_folder / "current"
