243ffcc12110c79240743300d6ebd39b680e866e
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-24 13:10:32 -0600

    fixed bugs with cron script

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-python /usr/src/app/python/api.py && python /usr/src/app/python/join_tables.py
+echo "starting pipeline..."
+/usr/local/bin/python /usr/src/app/python/api.py && /usr/local/bin/python /usr/src/app/python/join_tables.py
+echo "done!"
